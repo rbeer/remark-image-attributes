f5b5fcdfd9ccd3418b9d9c34ef0e777bd8c6f36a
Raphael Beer <raphael.beer@protonmail.com>
2020-03-22 16:30:43 +0100

    Add: example

---
+++ example/example.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const vfile = require("to-vfile");
+const remark = require("remark");
+const remarkParser = require("remark-parse");
+const remarkImageAttributes = require("../src/index.js");
+const visit = require("unist-util-visit");
+
+console.log("Reading ./markdown.md");
+const markdown = vfile.readSync(`${__dirname}/example.md`);
+
+console.log("Parsing");
+const markdownAST = remark()
+  .use(remarkParser, { position: false })
+  .use(remarkImageAttributes)
+  .parse(markdown);
+
+visit(markdownAST, "image", node => console.log(node));
